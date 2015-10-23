a7d71b9247c4831c8b99e12e56f261767cf2e907
Karla Fernandes <karla@vitaminak.com.br>
2015-10-23 13:25:04 -0200

    Enemies run

--- js/app.js
+++ js/app.js
@@ -17,6 +17,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+	var spaceSize = 50;
+	
+	// Putting the enemies with different speeds
+    if (this.x > 500) {
+        this.x = - spaceSize;
+	    this.speed = spaceSize * Math.floor(Math.random() * 7 + 2);
+    }
 }
 
 // Draw the enemy on the screen, required method for game
